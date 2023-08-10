5c80219c3327047e859ba4db6976628301b3f8da
wikikk <121543611+wikikk@users.noreply.github.com>
2023-08-10 17:54:22 +0800

    Add files via upload

---
+++ ocr.py
@@ -0,0 +1,19 @@
+import ddddocr,base64
+def ocr(imgb64,old = False):
+    if not imgb64:return {"code":-1,"msg":"img值未传送"}
+    ocr = ddddocr.DdddOcr()
+    try:
+        image = base64.b64decode(imgb64)
+        res = ocr.classification(image)
+        return {"code":0,"msg":"识别成功","result":res}
+    except Exception as e:
+        return {"code":-1,"msg":f"识别失败,原因:{str(e)}"}
+def slide(target_bytesb64,background_bytesb64):
+    try:
+        det = ddddocr.DdddOcr(det=False, ocr=False)
+        target_bytes,background_bytes = base64.b64decode(target_bytesb64),base64.b64decode(background_bytesb64)
+        res = det.slide_match(target_bytes, background_bytes)
+    except Exception as e:
+        return {"code":-1,"msg":f"识别失败,原因:{str(e)}"}
+    return {"code":0,"msg":"识别成功","result":res}
+ 
---
+++ server.py
@@ -0,0 +1,42 @@
+#-*- coding:utf-8 -*-
+from flask import Flask
+from flask import request as req
+from ocr import ocr,slide
+#设置端口
+port = 9898
+rightkey = 'zhishuiyyds'
+#—————————————为flask基础配置——————————————————————————
+class Config(object):
+    DEBUG=True
+    JSON_AS_ASCII=False
+    SECRET_KEY = '1933ef4c9d9ea88c51e5b38a8e2ae222f422e27b6982bea9eee748e5922fe08d'
+app = Flask(__name__)
+app.config.from_object(Config)
+@app.route('/ocr',methods=['GET','POST'], strict_slashes=False)
+def ocr():
+    try:
+        img = req.values["img"]
+        if req.values.get("old"):
+            return ocr(img,True)
+        return ocr(img)
+    except Exception as e:
+        return {"code":-1,"msg":str(e)}
+#—————————————为api辅助参数______________
+@app.route('/slide',methods=['GET','POST'], strict_slashes=False)
+def slide():
+    try:
+        img,img1 = req.values["img"],req.values["img1"]
+        return slide(img,img1)
+    except Exception as e:
+        return {"code":-1,"msg":str(e)}
+    
+ 
+@app.route('/ping', methods=['GET'])
+def ping():
+    return "pong"
+
+@app.errorhandler(404)
+def page_not_found(_):
+    return "欢迎使用ocr识别系统"
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=port)
